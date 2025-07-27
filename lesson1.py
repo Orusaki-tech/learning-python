3523921e0b85333c9aa4415babddad80f2957d6b
Okware Tom Polycarp <100290842+Orusaki-tech@users.noreply.github.com>
2025-07-27 23:49:23 +0000

    This is my first commit and i have added the variables.md file explaining where variables are stored.

---
+++ lesson1.py
@@ -0,0 +1,22 @@
+#Writing a python program
+# Python is Versatile, you can use it to do web development, data science, machine learning and more.
+# Python has a strong community. There is pretty much a python package for everything.
+# Python is easy to learn and concise.
+
+
+# We will create a program that tells you how old you are in decades and years.
+# This simple program teaaches you how to ask for user input, save the input to a variable, calculate the decades and years, convert the numbers to text, and print results to the screen.
+
+
+
+
+
+
+#Input and Outout
+
+#Data Types
+
+#Web Requests
+
+#installing and using python packages
+
